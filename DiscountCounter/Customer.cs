40a9ce8fd3a4130a5c02f9b9f0d71701afc0652e
TomaszR <tomeas23@email.com>
2023-06-16 14:29:18 +0200

    AgeFix

--- DiscountCounter/Customer.cs
+++ DiscountCounter/Customer.cs
@@ -1,11 +1,13 @@
-﻿namespace DiscountCounter
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscountCounter
 {
     public class Customer
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public required string Name { get; set; }
-
+        [Range(0,130)]
         public required int Age { get; set; }
 
         public required DateTime LastVisit { get; set; }
